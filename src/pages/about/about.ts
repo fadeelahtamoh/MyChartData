8573a52ede46b4e97eaac1ce254caded9c1d3dd4
fadeelah <s64309010014@ptnicec.ac.th>
2022-11-02 11:10:13 +0700

    ส่งครั้งที่1

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,6 +6,43 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels:string[] = ['2019', '2020', '2021', '2022'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+  
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81], label: 'จำนวนแรกเข้า'},
+    {data: [28, 48, 40, 19], label: 'จำนวนสำเร็จการศึกษา'}
+  ];
+  
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+  
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+  
+  public randomize():void {
+    // Only Change 3 values
+    let data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      (Math.random() * 100),
+      56,
+      (Math.random() * 100),
+      40];
+    let clone = JSON.parse(JSON.stringify(this.barChartData));
+    clone[0].data = data;
+    this.barChartData = clone;
+    
+  }
 
   constructor(public navCtrl: NavController) {
 
